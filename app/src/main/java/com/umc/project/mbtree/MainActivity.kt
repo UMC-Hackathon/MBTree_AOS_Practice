e931b59e2629dc3e7fbc9b810945d735cbaedf06
이어진 <leeeojin6@gmail.com>
2022-07-02 21:22:44 +0900

    어진이 코드

--- app/src/main/java/com/umc/project/mbtree/MainActivity.kt
+++ app/src/main/java/com/umc/project/mbtree/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.umc.project.mbtree
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    fun onClick(view: View) { val dialogFragment: DialogFragment = GPSDialogFragment()
+        when (view.id) {
+            R.id.button1 -> //                다이얼로그 표시, 프래그먼트를 사용하므로 FragmentManager 를 전달
+                dialogFragment.show(supportFragmentManager, "gps dilog")
+        }
+    }
+
+
 }
